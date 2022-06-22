aa05ee5694d741c03f2af47681d0e8dfcf9b3d36
Niko Rivas <rivasniko@gmail.com>
2022-06-21 20:12:40 -0700

    add basic html and javascript

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+// User Interface Logic
+function handleForm(event) {
+  event.preventDefault();
+  const userSelections = document.querySelectorAll(
+    'input[name=transportation-option]:checked'
+  );
+  // we'll add more code to display results here!
+}
+
+window.addEventListener('load', function () {
+  document
+    .querySelector('form#transportation_survey')
+    .addEventListener('submit', handleForm);
+});
