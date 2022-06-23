f3691041b00c41f929ebad9dfbac14fecec47ae0
Niko Rivas <rivasniko@gmail.com>
2022-06-22 17:42:15 -0700

    add for each loop so that ddata displays upon submit

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,16 @@
+// User Interface Logic
 function handleForm(event) {
   event.preventDefault();
   const userSelections = document.querySelectorAll(
     'input[name=transportation-option]:checked'
   );
-  // we'll add more code to display results here!
+  const userSelectionsArray = Array.from(userSelections);
+
+  userSelectionsArray.forEach(function (element) {
+    const paragraph = document.createElement('p');
+    paragraph.append(element.value);
+    document.body.append(paragraph);
+  });
 }
 
 window.addEventListener('load', function () {
